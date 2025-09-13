d26414dcc9be7fdec32a8ae077a2ff8b5964c813
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-13 17:21:58 +0300

    Added a Text View and a Button to the .xib

--- ChuckNorrisJokes-iOS-App/JokeViewController.swift
+++ ChuckNorrisJokes-iOS-App/JokeViewController.swift
@@ -1,10 +1,16 @@
 import UIKit
 
-class JokeViewController: UIViewController {
+final class JokeViewController: UIViewController {
 
+    @IBOutlet private weak var jokeTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load from: https://api.chucknorris.io/jokes/random
+    }
+    
+    @IBAction private func loadJokeTapped() {
+        print("Load Joke")
     }
 }
