ec2d4db980b93f94d276315a21de317489f326ad
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-13 17:15:24 +0300

    Some basic cleanps

---
+++ ChuckNorrisJokes-iOS-App/JokeViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class JokeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load from: https://api.chucknorris.io/jokes/random
+    }
+}
