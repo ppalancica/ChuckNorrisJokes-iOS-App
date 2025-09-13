9d4bf306a0528b4ee734bccf2f2365a23739afda
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-13 17:39:22 +0300

    Added remote JSON loading functionality

--- ChuckNorrisJokes-iOS-App/JokeViewController.swift
+++ ChuckNorrisJokes-iOS-App/JokeViewController.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 final class JokeViewController: UIViewController {
+    
+    private var session: URLSession!
+    private var dataTask: URLSessionDataTask!
 
     @IBOutlet private weak var jokeTextView: UITextView!
     
@@ -11,6 +14,47 @@ final class JokeViewController: UIViewController {
     }
     
     @IBAction private func loadJokeTapped() {
-        print("Load Joke")
+        dataTask?.cancel()
+        
+        if session == nil {
+            session = URLSession(configuration: URLSessionConfiguration.default,
+                                 delegate: self,
+                                 delegateQueue: nil)
+        }
+        
+        let url = URL(string: "https://api.chucknorris.io/jokes/random")!
+        let request = URLRequest(url: url)
+        
+        dataTask = session.dataTask(with: request) { data, response, error in
+            if let error {
+                self.handleCompletion(error: error.localizedDescription, data: nil)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200..<300).contains(response.statusCode) else {
+                self.handleCompletion(error: "Invalid response", data: data)
+                return
+            }
+            
+            guard let data else {
+                self.handleCompletion(error: "Invalid data", data: nil)
+                return
+            }
+            
+            let jsonAsString = String(data: data, encoding: .utf8)
+            
+            print(jsonAsString ?? "Empty")
+        }
+        
+        dataTask.resume()
     }
+    
+    private func handleCompletion(error: String?, data: Data?) {
+        
+    }
+}
+
+extension JokeViewController: URLSessionDelegate {
+    
 }
