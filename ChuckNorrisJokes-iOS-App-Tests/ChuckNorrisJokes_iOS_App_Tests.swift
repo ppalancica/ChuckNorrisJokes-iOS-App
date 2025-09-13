b4847524818c6542ce61037ba2c3b5199c743b21
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-13 18:04:33 +0300

    Added a basic test

--- ChuckNorrisJokes-iOS-App-Tests/ChuckNorrisJokes_iOS_App_Tests.swift
+++ ChuckNorrisJokes-iOS-App-Tests/ChuckNorrisJokes_iOS_App_Tests.swift
@@ -1,4 +1,18 @@
 @testable import ChuckNorrisJokes_iOS_App
 import XCTest
 
-final class ChuckNorrisJokes_iOS_App_Tests: XCTestCase {}
+final class ChuckNorrisJokes_iOS_App_Tests: XCTestCase {
+    
+    func test_init_doesNotCrash() {
+        let _ = makeSUT()
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> JokeViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let jokeVC = storyboard.instantiateInitialViewController() as! JokeViewController
+        
+        return jokeVC
+    }
+}
