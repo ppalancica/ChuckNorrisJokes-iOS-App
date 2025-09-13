047927b90e7bc05271c7cb4dabb1a15dea5f87ea
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-13 17:10:46 +0300

    First commit. Created Xcode project from iOS App template

---
+++ ChuckNorrisJokes-iOS-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokes-iOS-App
+//
+//  Created by Pavel Palancica  on 13.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
